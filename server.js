1592c8991d563d710f3b5d01c54296c5e3a8bcf9
abubakrDeveloper <abubakriorashidof@gmail.com>
2024-12-26 22:55:13 +0500

    authCTRL boshlang'ich qismi boshlandi

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`server started ${PORT} on port...`)
-)
-// const MONGO_URL = process.env || MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.get((req, res) => {
-  send("new project: this is home page");
+app.get("/", (req, res) => {
+  send("inspa-app: this is home gape");
 });
 
-
-
-// mongoose.connect(MONGO_URL).then(() => {
-//   app.listen(PORT, () => console.log(`server started ${PORT} on port...`)
-//   )
-// })
+mongoose.connect(MONGO_URL).then(() => {
+  app.listen(PORT, () => console.log(`server started ${PORT} on port...`)
+  )
+})
---
+++ src/controllers/authCtrl.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const JWT_SECRET = process.env.JWT_SECRET;
+const Users = requrie("../models/userModel");
+
+const authCtrl = {
+  signUp: async (req, res) => {
+
+  },
+
+  lpogin: async (req, res ) => {
+    
+  }
+}
